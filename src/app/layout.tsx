d49279842e55583e827a2563b4558c7237aad44a
Adegboyega <gboyega.banjo@gmail.com>
2023-08-07 23:51:59 +0100

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../styles/styles.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}  boxed-style`}>
+        {children}
+      </body>
     </html>
   )
 }
