81eb5e6eb49370dd5fa1c3fbfe22a61ec04cf69f
Adegboyega <gboyega.banjo@gmail.com>
2023-09-11 15:08:33 +0100

    gtag

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -1,5 +1,7 @@
 export const pageview = (ga_id: string, url: string) => {
-  window.gtag("config", ga_id, {
-    page_path: url,
-  })
+  if (typeof window !== "undefined") {
+    window.gtag("config", ga_id, {
+      page_path: url,
+    })
+  }
 }
