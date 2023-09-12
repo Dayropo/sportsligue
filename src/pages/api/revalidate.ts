39b4f4a3ee5b7822e76681336b65d6c46d93256e
Adegboyega <gboyega.banjo@gmail.com>
2023-09-12 23:32:46 +0100

    on demand revalidation

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,39 @@
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook"
+import { NextApiRequest, NextApiResponse } from "next"
+
+const SANITY_WEBHOOK_SECRET = process.env.SANITY_WEBHOOK_SECRET
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME]
+  const isValid = isValidSignature(
+    JSON.stringify(req.body),
+    signature,
+    SANITY_WEBHOOK_SECRET
+  )
+
+  console.log(`===== Is the webhook request valid? ${isValid}`)
+
+  // Validate signature
+  if (!isValid) {
+    res.status(401).json({ success: false, message: "Invalid signature" })
+    return
+  }
+
+  try {
+    // const pathToRevalidate = req.body.slug.current
+
+    // console.log(`===== Revalidating: ${pathToRevalidate}`)
+    console.log(" ======= Revalidating ====== ")
+
+    await res.revalidate("/")
+
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // Could not revalidate. The stale page will continue to be shown until
+    // this issue is fixed.
+    return res.status(500).send("Error while revalidating")
+  }
+}
