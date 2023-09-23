ec090e24ebf2c390d3ade5046437f0f9732a8210
Adegboyega <gboyega.banjo@gmail.com>
2023-09-23 02:21:45 +0100

    optional chaining in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default async function sitemap() {
 
   // posts
   const posts = await getAllPosts()
-  const postUrls = posts.map(post => {
+  const postUrls = posts?.map(post => {
     return {
       url: `${baseUrl}/${post.slug.current}`,
       lastModified: new Date(post.publishedAt),
@@ -14,7 +14,7 @@ export default async function sitemap() {
 
   //categories
   const categories = await getAllCategories()
-  const categoryUrls = categories.map(category => {
+  const categoryUrls = categories?.map(category => {
     return {
       url: `${baseUrl}/category/${category.slug.current}`,
       lastModified: new Date(),
@@ -22,8 +22,8 @@ export default async function sitemap() {
   })
 
   // subcategories
-  const subCategoryUrls = categories.map(category => {
-    return category.subCategories.map(subCat => {
+  const subCategoryUrls = categories?.map(category => {
+    return category.subCategories?.map(subCat => {
       return {
         url: `${baseUrl}/category/${category.slug.current}/${subCat.slug.current}`,
         lastModified: new Date(),
@@ -38,5 +38,6 @@ export default async function sitemap() {
     },
     ...postUrls,
     ...categoryUrls,
+    ...subCategoryUrls,
   ]
 }
