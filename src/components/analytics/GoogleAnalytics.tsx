2c84230327a3084da2134cc504965bac248df372
Adegboyega <gboyega.banjo@gmail.com>
2023-10-01 16:04:29 +0100

    ads.txt

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -41,10 +41,11 @@ export default function GoogleAnalytics({
         }}
       />
       <Script
+        id="adsbygoogle-init"
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1384009601184330"
         crossOrigin="anonymous"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
       />
     </>
   )
