1173f629bd667d73e2456b2acf8d9ab29f264af0
Adegboyega <gboyega.banjo@gmail.com>
2023-10-18 08:59:31 +0100

    metadata alterations

---
+++ src/app/manifest.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next"
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Sportsligue",
+    short_name: "Sportsligue",
+    description:
+      "Stay updated with the latest sports news, scores, and highlights on Sportsligue.com",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      { src: "/icon-192.png", type: "image/png", sizes: "192x192" },
+      { src: "/icon-512.png", type: "image/png", sizes: "512x512" },
+    ],
+  }
+}
