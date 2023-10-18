8540f4fc96b89d27acf5c98d6241d45a1d885eb5
Adegboyega <gboyega.banjo@gmail.com>
2023-10-18 21:22:42 +0100

    reduce margin for paragraphs

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,15 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../../styles/styles.css"
 // import "../../styles/_general.scss"
 import type { Metadata } from "next"
-import { Roboto_Condensed } from "next/font/google"
 import Script from "next/script"
 import { NextAuthProvider } from "@/src/components/auth/Providers"
 import GoogleAnalytics from "@/src/components/analytics/GoogleAnalytics"
-
-const robotoCondensed = Roboto_Condensed({
-  weight: ["300", "400", "700"],
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.sportsligue.com"),
@@ -35,7 +29,7 @@ export default function RootLayout({
         <GoogleAnalytics GA_MEASUREMENT_ID="G-0ZER7XKZDG" />
       )}
 
-      <body className={`${robotoCondensed.className}  boxed-style`}>
+      <body className="boxed-style">
         <NextAuthProvider>{children}</NextAuthProvider>
 
         <Script src="https://code.jquery.com/jquery-3.4.1.min.js" />
