1593ab86666584bf426fd8d160e3381d30a0e929
Adegboyega <gboyega.banjo@gmail.com>
2024-07-09 21:06:45 +0100

    ad publisher

--- src/components/adpublisher/Tag300x250.tsx
+++ src/components/adpublisher/Tag300x250.tsx
@@ -1,10 +1,10 @@
 import Script from "next/script"
-import React, { useEffect } from "react"
+import React from "react"
 
 const Tag300x250 = () => {
   return (
     <div id="div-gpt-ad-1720450834557-0" style={{ minWidth: "300px", minHeight: "250px" }}>
-      <Script>
+      <Script id="ad-300x250">
         {`
             googletag.cmd.push(function() { googletag.display('div-gpt-ad-1720450834557-0'); });
 
--- src/components/adpublisher/Tag300x600.tsx
+++ src/components/adpublisher/Tag300x600.tsx
@@ -4,7 +4,7 @@ import Script from "next/script"
 const Tag300x600 = () => {
   return (
     <div id="div-gpt-ad-1720451125756-0" style={{ minWidth: "300px", minHeight: "600px" }}>
-      <Script>
+      <Script id="ad-300x600">
         {`
            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1720451125756-0'); });
 
